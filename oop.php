e625c69f0410288c695b840772c83c5cb3342d05
masud31058 <masud31058@gmail.com>
2024-08-26 20:21:34 +0600

    oop.php

--- oop.php
+++ oop.php
@@ -41,14 +41,6 @@ class Member {
         return $this->name;
     }
 
-    public function borrowBook(Book $book) {
-        if ($book->borrowBook()) {
-            echo $this->name . " borrowed '" . $book->getTitle() . "'.\n";
-        } else {
-            echo $this->name . " could not borrow '" . $book->getTitle() . "' because it is not available.\n";
-        }
-    }
-
     public function returnBook(Book $book) {
         $book->returnBook();
         echo $this->name . " returned '" . $book->getTitle() . "'.\n";
@@ -61,8 +53,6 @@ $book2 = new Book("To Kill a Mockingbird", 3);
 $member1 = new Member("John Doe");
 $member2 = new Member("Jane Smith");
 
-$member1->borrowBook($book1); 
-$member2->borrowBook($book2); 
 
 echo "Available Copies of '" . $book1->getTitle() . "': " . $book1->getAvailableCopies() . "\n";
 echo "Available Copies of '" . $book2->getTitle() . "': " . $book2->getAvailableCopies() . "\n";
